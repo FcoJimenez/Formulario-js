edc9c75406efea2dc5000366750ca850d4a0cb51
Francisco Jiménez A <90414185+FcoJimenez@users.noreply.github.com>
2021-09-16 15:36:18 -0300

    Update forms.js

--- forms.js
+++ forms.js
@@ -1,6 +1,8 @@
 $('form').submit(function(event){    
     var userResponses = $(this).serialize()
-    console.log(userResponses);
+    //event.preventDefault()                      //detiene el envio para tomar los valores, cambia el contexto
+    //console.log(this)                         //muestra el objeto (this) <- form
+    console.log(userResponses);                 //la f() serialize extrae la cadena con los name y sus valores
 })
 
 $('button').click(function(event){
